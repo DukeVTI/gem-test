253abf3b7d0462d809ed5a7265de42a4d1913f01
Anon <virusiatechindustries@gmail.com>
2025-01-13 03:38:57 -0800

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import App from './App'
-/* import ModernCyberTech from './Jimmy/jimmy' */
+import ModernCyberTech from './Jimmy/Jimmy'
 import ModernCyberTechV2 from './Jimmy/Jimmy1'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ModernCyberTechV2/>
+    <ModernCyberTech/>
   </StrictMode>,
 )
